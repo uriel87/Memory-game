add9501620b6dc2f996fb0295f885e1f2aa6bf66
uriel87 <urielch87@gmail.com>
2017-12-03 20:54:26 +0200

    fix game

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use('/css', express.static(__dirname + './js'));
 app.use('/js', express.static(__dirname + './css'));
 app.use('/data', express.static(__dirname + './data'));
 app.use('/images', express.static(__dirname + './images'));
-// app.use('/views', express.static(__dirname + './views'));
 
 
 app.use('/', express.static('./')).listen(process.env.PORT || 8080);
